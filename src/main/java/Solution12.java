189c9848226270c4729a395ad9969696845e1493
Almond64 <almond64@knights.ucf.edu>
2021-09-19 23:26:41 -0400

    added name

--- src/main/java/Solution12.java
+++ src/main/java/Solution12.java
@@ -1,3 +1,8 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 2 Solution
+ * Copyright 2021 Paul Kelly
+ */
+
 import java.util.Scanner;
 
 public class Solution12 {
@@ -11,6 +16,11 @@ public class Solution12 {
         int years = input.nextInt();
         //calculate the investment total
         double total = principal * (1 + (rate / 100)*years);
-        System.out.printf("After %d years at %.2f, the investment will be worth $%.2f\n", years, rate, total);
+        /*
+        Separate the change from the total, then Math.ceil(change*100).
+        Divide this value by 100 and then add it back in the total.
+         */
+        System.out.printf("Total %f\n", total);
+        //System.out.printf("After %d years at %.2f, the investment will be worth $%.2f\n", years, rate, total);
     }
 }
